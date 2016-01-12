8a38afb217b9c307cec16b4b24cb431d1e6c6a99
Jeremy <jeremy.devouassoux@gmail.com>
2016-01-12 17:26:11 +0100

    add logger

---
+++ logger.py
@@ -0,0 +1,45 @@
+class Logger:
+	""" logger """
+	modes = ["ERROR", "INFO ", "DEBUG"]
+
+	def __init__(self, logInFile=False):
+
+		self.logInFile = False
+		self.isFileOpen = False
+		if type(logInFile) is bool:
+			self.logInFile = logInFile
+
+	def setMode(self, mode="ERROR"):
+		if mode in Logger.modes :
+			self.mode = mode
+		else :
+			self.mode = Logger.modes[0]
+
+		self.info("logger's mode : "+self.mode)
+
+	def debug(self, message):
+		self._log(message, Logger.modes[2])
+
+	def info(self, message):
+		self._log(message, Logger.modes[1])
+
+	def error(self, message):
+		self._log(message, Logger.modes[0])
+
+	def _log(self, message, mode):
+		if mode in Logger.modes and Logger.modes.index(mode) <= Logger.modes.index(self.mode) :
+			if self.logInFile:
+				self._printInFile("{} : {}".format(mode, message), mode)
+			else :
+				print("{} : {}".format(mode, message))
+
+	def _printInFile(self, message, mode):
+		print("Should be logged in a file")
+		print("{} : {}".format(mode, message))
+
+	def close(self):
+		if self.logInFile and self.isFileOpen:
+			print("should close the log file")
+
+		
+#		print("log in file ? {}".format(self.logInFile))
